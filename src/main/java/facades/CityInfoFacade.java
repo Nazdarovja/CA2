452025cf2924fd0afdc26353c6570ded1d225571
ML <Mellem@MLs-MacBook-Pro.local>
2018-03-14 13:48:16 +0100

    removed

--- src/main/java/facades/CityInfoFacade.java
+++ src/main/java/facades/CityInfoFacade.java
@@ -5,10 +5,53 @@
  */
 package facades;
 
+import facades.interfaces.CRUDInterface;
+import java.util.List;
+
 /**
  *
  * @author Mellem
  */
 public class CityInfoFacade implements CRUDInterface {
+
+    @Override
+    public Object create(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object read(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object read(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object update(Long id, Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object update(String id, Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object delete(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
